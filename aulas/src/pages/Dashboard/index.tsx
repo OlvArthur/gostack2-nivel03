7a67769774791dc39b612b77970a40f4889de396
Arthur Oliveira <olvarthur@gmail.com>
2020-04-22 11:58:14 -0300

    Using localStorage

--- aulas/src/pages/Dashboard/index.tsx
+++ aulas/src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
+import Repository from '../Repository';
 
 interface Repository {
   full_name: string;
@@ -18,7 +19,24 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storedRepository = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    );
+
+    if (storedRepository) {
+      return JSON.parse(storedRepository);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
