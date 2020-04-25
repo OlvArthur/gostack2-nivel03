3d9ce4ca7a5f8123338278666d38dc03a683abf8
Arthur Oliveira <olvarthur@gmail.com>
2020-04-25 15:19:35 -0300

    Creating SigUp Page

--- iniciando-front-end-web/aulas/src/App.tsx
+++ iniciando-front-end-web/aulas/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SigIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SigIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
